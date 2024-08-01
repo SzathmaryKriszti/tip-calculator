289abf7cfc88368bd11992eb248158e9154daf52
SzathmaryKriszti <krisztacsuri@gmail.com>
2024-08-01 10:39:35 +0200

    create two-way binding

--- src/components/Tip-Calculator/Input.tsx
+++ src/components/Tip-Calculator/Input.tsx
@@ -7,7 +7,10 @@ export default function input({bill, onSetBill} : InputProps) {
   return (
     <div>
       <label className="form-label">How much was the bill?</label>
-      <input className="form-control" type="text" placeholder="Bill value" />
+      <input
+          value={bill}
+          onChange={(event) => onSetBill(Number(event.target.value))}
+          className="form-control" type="text" placeholder="Bill value" />
     </div>
   );
 }
