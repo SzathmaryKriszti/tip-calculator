04f82c393ad069e1399d813b60c87003160b936c
SzathmaryKriszti <krisztacsuri@gmail.com>
2024-08-01 10:36:28 +0200

    create states pass state and set method to Input.tsx

--- src/components/Tip-Calculator/Input.tsx
+++ src/components/Tip-Calculator/Input.tsx
@@ -1,4 +1,9 @@
-export default function input() {
+interface InputProps{
+    bill: number;
+    onSetBill: (bill: number) => void;
+}
+
+export default function input({bill, onSetBill} : InputProps) {
   return (
     <div>
       <label className="form-label">How much was the bill?</label>
--- src/components/Tip-Calculator/TipCalculator.tsx
+++ src/components/Tip-Calculator/TipCalculator.tsx
@@ -2,15 +2,22 @@ import Input from './Input';
 import Output from './Output';
 import Reset from './Reset';
 import Select from './Select';
+import {useState} from "react";
 
 export default function TipCalculator() {
+  const [bill, setBill] = useState(0);
+  const [percentage1, setPercentage1] = useState(0);
+  const [percentage2, setPercentage2] = useState(0);
+
+  const tip = bill * ((percentage1 + percentage2) /2 / 100);
+
   return (
     <div>
       <div className="card" style={{ width: '18rem' }}>
         <div className="card-body">
           <h5 className="card-title text-center">Tip Calculator</h5>
           <div className="text-center">
-            <Input />
+            <Input bill={bill} onSetBill={setBill} />
             <Select />
             <Select />
             <Output />
